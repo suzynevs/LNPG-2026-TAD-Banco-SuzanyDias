002339ecb46be5c2e9ec6e98037c3841a4503dee
suzynevs <svdn2@aluno.ifal.edu.br>
2026-06-02 20:11:53 -0300

    client

--- src/cliente.py
+++ src/cliente.py
@@ -17,4 +17,24 @@ class Cliente:
             self.__telefone = somente_digitos(telefone)
 
         if email is not None validar_email(email):
-          self.__ 
+          self.__email = email 
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_cpf(self):
+        return self.__cpf
+
+    def get_telefone(self):
+        return self.__telefone
+
+    def get_email(self):
+        return self.__email
+
+    def alterar_telefone(self, telefone):
+        if validar_telefone(telefone):
+            self.__telefone = somente_digitos(telefone)
+
+    def alterar_email(self, email):
+        if validar_email(email):
+            self.__email = email
